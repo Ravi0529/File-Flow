ab51852765e22d8da73d284b3390b3632dd0cb86
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-06-19 11:16:25 +0530

    setting-image(#4)

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -10,16 +10,10 @@ const Dropzone = ({ onDrop }) => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-white border-4 border-dashed border-gray-400 rounded-lg p-8 w-full max-w-lg mx-auto cursor-pointer"
+      <div className="flex flex-col items-center justify-center bg-white border-4 border-dashed border-gray-400 rounded-lg p-3 w-full max-w-lg mx-auto cursor-pointer"
         {...getRootProps()}
-        style={{
-          backgroundImage: `url(${Upload})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'contain',
-          height: '120px',
-        }}
       >
+        <img src={Upload} className='h-20' alt="uploading-image" />
         <input {...getInputProps()} />
       </div>
         <p className="text-white text-center mt-2">Drag & drop files here, or click to select files</p>
